1e71ab592b0c028f596d23ebb0f2729f88917162
NethmiJayarathna <it21208812@my.sliit.lk>
2024-01-15 01:28:30 +0530

    delete

--- Ecommerce/routes/authRoute.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const {createUser, loginUserCtrl, getallUser} = require("../controller/userCtrl");
-const router = express.Router();
-router.post("/register" , createUser); 
-router.post("/login", loginUserCtrl);
-router.get("/all-users", getallUser);
-module.exports = router;
-
