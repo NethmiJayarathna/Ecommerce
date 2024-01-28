e9088e171401a8ca70b1ad088d1f9d8e66549b12
NethmiJayarathna <it21208812@my.sliit.lk>
2024-01-29 00:15:43 +0530

    updates

--- Ecommerce/models/productModel.js
+++ Ecommerce/models/productModel.js
@@ -22,16 +22,16 @@ var productSchema = new mongoose.Schema({
         required:true,
     },
     category:{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "Category",
+        type: String,
+        required: true,
     }, 
     brand: {
         type: String,
-        enum: ["Apple", "Samsung", "Lenovo"],
+        required: true,
     }, 
     quantity: {
-    type: Number,
-    require: true,
+        type: Number,
+        require: true,
     },
     sold: {
         type: Number,
@@ -42,7 +42,7 @@ var productSchema = new mongoose.Schema({
     },
     color:{
         type: String,
-        enum:["Black", "Brown", "Red"],
+        required: true,
     },
     ratings:[{
         star: Number,
--- Ecommerce/routes/productRoute.js
+++ Ecommerce/routes/productRoute.js
@@ -1,7 +1,18 @@
 const express = require("express");
-const { createProduct } = require("../controller/productCtrl");
-const router = require("./authRoute");
+const { 
+    createProduct, 
+    getaProduct, 
+    getAllProduct, 
+    updateProduct, 
+    deleteProduct 
+} = require("../controller/productCtrl");
+const { isAdmin, authMiddleware } = require("../middlewares/authMiddleware")
+const router = express.Router();
 
-router.post("/", createProduct);
+router.post("/",authMiddleware, isAdmin, createProduct);
+router.get("/:id", getaProduct);
+router.put("/:id",authMiddleware, isAdmin, updateProduct);
+router.delete("/:id",authMiddleware, isAdmin, deleteProduct);
+router.get("/", getAllProduct  );
 
 module.exports = router;
